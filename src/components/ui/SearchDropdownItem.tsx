1eb1b2e6f74cffd5e1c2bce21966b6e34982f1f1
ngapham <nga.pham.cs83@gmail.com>
2025-11-03 17:51:54 +0700

    fix bug in searchdropdownitem

--- src/components/ui/SearchDropdownItem.tsx
+++ src/components/ui/SearchDropdownItem.tsx
@@ -1,4 +1,4 @@
-﻿import { Eye, HandHeart, Sparkles, Bath, Palette, Flame, Hand, LucideIcon } from "lucide-react";
+﻿import { Eye, HandHeart, Sparkles, Bath, Palette, Flame, Hand, PersonStanding, SmilePlus, Syringe, BriefcaseMedical, LucideIcon } from "lucide-react";
 import { Dropdown } from "react-bootstrap";
 
 // interface of dropdown items
@@ -10,13 +10,15 @@ export interface DropdownItemProps {
 
 // Map categories to lucide-react icons
 export const categoryIconMap: Record<string, LucideIcon> = {
+    "Body": PersonStanding,
     "Eyebrows and eyelashes": Eye,
-    "Massage": HandHeart,
-    "Nail": Hand,
-    "Body": Sparkles,
-    "Spa packages": Bath,
+    "Facials and Skincare": SmilePlus,
+    "Hair and Styling": Sparkles, 
+    "Injectables and fillers": Syringe, 
     "Makeup": Palette,
-    "Waxing": Flame,
+    "Massage": HandHeart,
+    "Medical and dental": BriefcaseMedical,
+    "Nails": Hand,
 };
 
 const DropdownItem = ({ icon: Icon, category, onClick }: DropdownItemProps) => {
