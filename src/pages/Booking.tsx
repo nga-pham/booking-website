403df982a7d05dc179c455702b688275043fbbcc
ngapham <nga.pham.cs83@gmail.com>
2025-11-07 10:16:15 +0700

    handle book services

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,10 +1,10 @@
-import { ArrowLeft, X } from "lucide-react";
+import { ArrowLeft, X, ChevronRight } from "lucide-react";
 import { useEffect, useState } from 'react';
-import { Col, Container, Navbar, Row } from "react-bootstrap";
+import { Col, Container, Navbar, Row, Card, Button, Image } from "react-bootstrap";
 import { useNavigate, useParams } from 'react-router-dom';
 import ServiceTabs from "../components/ui/ServiceTabs";
 import { partnerDataWithId } from "../lib/utils";
-
+import StarRating from "../components/ui/StarRating"
 
 interface chosenOptionProps {
     services: string[];
@@ -58,15 +58,53 @@ const MainContent = ({ id }) => {
                 </Col>
                 <Col lg={4}>
                     {/*booking information*/}
-                    {chosenServices.length > 0 ? (
-                        chosenServices.map((service) => {
-                            return (
-                                <p key={service}>{service}</p>
-                            )
-                        })
-                    ) : (
-                        <p>No service selected</p>
-                    )}
+                    <Card className="mt-5 px-0 py-2">
+                        <Card.Body>
+                            <Container>
+                                <Row className="text-start">
+                                    <Col lg="3" className=""><Image rounded width="80px" height="80px" src={currentPartner.photos[0]} /></Col>
+                                    <Col lg="9">
+                                        <p style={{ fontSize: '1.1rem' }}><strong>{currentPartner.name}</strong></p>
+                                        <p style={{ fontSize: '1.1rem' }}>
+                                            <strong>{currentPartner.rating}</strong>
+                                            <StarRating />({currentPartner.numberOfRating})
+                                        </p>
+                                        <p style={{ color: 'rgba(0,0,0,0.5)' }}>{currentPartner.address}</p>
+                                    </Col>
+                                </Row>
+                                <Row className="text-start">
+                                    {chosenServices.length > 0 ? (
+                                        chosenServices.map((service) => {
+                                            return (
+                                                <Row key={service}>
+                                                    <Col className="text-start">{service}</Col>
+                                                    <Col className="text-end">Price</Col>
+                                                </Row>
+                                            )
+                                        })
+                                    ) : <p style={{ color: 'rgba(0,0,0,0.5)' }}>No service selected</p>
+                                    }
+                                </Row>
+                            </Container>
+                            
+                        </Card.Body>
+                        <Card.Footer className="justify-content-center">
+                            {chosenServices.length > 0 ? 
+                                <Button variant="primary" size="lg" className="d-flex align-items-center ml-2 rounded-pill"
+                                    style={{ backgroundColor: 'black', color: "white" }}
+                                >
+                                    Continue <ChevronRight size={20} />
+                                </Button>
+                                :
+                                <Button variant="primary" size="lg" className="d-flex align-items-center ml-2 rounded-pill"
+                                    style={{ backgroundColor: 'black', color: "white" }} disabled
+                                >
+                                Continue <ChevronRight size={20} />
+                            </Button>
+                            }
+                        </Card.Footer>
+                    </Card>
+                    
                 </Col>
             </Row>
         </Container>
