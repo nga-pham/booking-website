a9bb47364df3e9623ce884c4317f6c51c445a6b2
ngapham <nga.pham.cs83@gmail.com>
2025-11-08 08:50:30 +0700

    handle checked service

--- src/components/ui/ServiceCard.tsx
+++ src/components/ui/ServiceCard.tsx
@@ -1,5 +1,5 @@
 ﻿import { Card, Container, Row, Col, Button } from "react-bootstrap";
-import { Plus, Save } from 'lucide-react';
+import { Plus, Check } from 'lucide-react';
 import { useState, useEffect } from 'react'
 import { chosenServiceProps } from "./Interfaces"
 
@@ -17,19 +17,27 @@ interface ServiceCardProps {
 const ServiceCard = ({ id, name, duration, cost, isBookingPage, sendDataToTabs }: ServiceCardProps) => {
     // Save service to book and send to tabs
     const [savedService, setSavedService] = useState<chosenServiceProps>(undefined)
+    // change style when chosen
+    const [isChosen, setIsChosen] = useState<boolean>(undefined)
+
     const getChosenService = (service) => {
         setSavedService(service)
+        setIsChosen(true)
     }
 
+
     useEffect(() => {
+        console.log(isChosen)
         sendDataToTabs(savedService)
     }, [savedService])
 
-    return (
+    
+
+    if (!isChosen) return (
         <Card style={{ width: "50rem", borderRadius: '1rem' }} className="mt-3 py-0 px-2" key={id}>
             <Card.Body>
                 <Container>
-                    <Row >
+                    <Row>
                         <Col>
                             <Card.Title><p style={{ fontSize: '1.1rem' }}>{name}</p></Card.Title>
                             <Card.Text style={{ color: 'rgba(0,0,0,0.5)', fontSize: '1rem' }}>{duration}</Card.Text>
@@ -43,6 +51,27 @@ const ServiceCard = ({ id, name, duration, cost, isBookingPage, sendDataToTabs }
                                 <Plus size={20} />
                             </Button>
                         </Col>}
+                    </Row>
+                </Container>
+            </Card.Body>
+        </Card>
+    )
+
+    else return ( // green bordered card with checked item
+        <Card style={{ width: "50rem", borderRadius: '1rem', border: "2px solid #3b8132" }} className="mt-3 py-0 px-2" key={id}>
+            <Card.Body>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card.Title><p style={{ fontSize: '1.1rem' }}>{name}</p></Card.Title>
+                            <Card.Text style={{ color: 'rgba(0,0,0,0.5)', fontSize: '1rem' }}>{duration}</Card.Text>
+                            <Card.Text style={{ fontSize: '1rem' }}>{cost.toString()} VND</Card.Text>
+                        </Col>
+                        {/* make the height stretch full height; right-align vertical-center the plus icon */}
+                        {isBookingPage && <Col className="d-flex align-items-center justify-content-end h-100">
+                            <div style={{ width: '40px', height: '32px', backgroundColor: "#F5F5F5", borderRadius: '50%', paddingLeft: '0.5rem' }}><Check size={20} /></div>
+                        </Col>
+                        }
                     </Row>
                 </Container>
             </Card.Body>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    //base: "/booking-website/", // <-- repo name + trailing slash,
     plugins: [
         plugin(),
     ],
